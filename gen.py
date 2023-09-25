a8fb7bed5cf658a71e8461ee30986559deddabd9
Di Nguyen <dingyen96@gmail.com>
2023-09-24 22:54:07 -0400

    Updated gen.py

--- gen.py
+++ gen.py
@@ -1,10 +1,12 @@
 import random
+import argparse
 from funcs import *
+from mapping import *
 
-# Generates n random pairs of input - output for function func that takes in a list of params numbers
-def generate_random_IO_pairs(n, func, params):
+# Generates tests random IO pairs for function func that takes in a list of params numbers
+def generate_random_IO_pairs(func, params, tests):
     io_pairs = []
-    for _ in range(n):
+    for _ in range(tests):
         arg_list = []
         for _ in range(params):
             x = random.randint(-10000, 10000)
@@ -21,13 +23,29 @@ def generate_check_expect(func_name, arg_lists, expected_results):
         check_expects.append(check_expect)
     return '\n'.join(check_expects)
 
-random_pairs = generate_random_IO_pairs(1000, median, 3)
+# Create parser object to read command line arguments
+parser = argparse.ArgumentParser(description = "Racket Check-expect Generator")
 
+# Add command line arguments
+parser.add_argument("func", nargs = 1, metavar = "func [str]", type = str, help = "Function to generate check-expects for")
+parser.add_argument("params", nargs = 1, metavar = "params [int]", type = int, help = "Number of parameters of the function")
+parser.add_argument("tests", nargs = 1, metavar = "tests [int]", type = int, help = "Number of check-expects to be generated")
+
+# Parse the command line arguments
+cmd_line_args = parser.parse_args()
+func_name = cmd_line_args.func[0]
+func = function_mappings[cmd_line_args.func[0]]
+params = cmd_line_args.params[0]
+tests = cmd_line_args.tests[0]
+
+# Generate random IO pairs
+random_pairs = generate_random_IO_pairs(func, params, tests)
+
+# Generate argument lists and expected results
 arg_lists, expected_results = zip(*random_pairs)
 
-check_expects = generate_check_expect('median', arg_lists, expected_results)
-
-# print(check_expects)     
+# Generate list of check-expects
+check_expects = generate_check_expect(func_name, arg_lists, expected_results)
 
 # Put the check-expects in check-expect.txt
 with open('check-expect.txt', 'w') as file:
